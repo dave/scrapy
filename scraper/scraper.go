988793916d971e369f2d0ee1965719e85e1fd96c
Dave Brophy <dave@brophy.uk>
2018-08-29 18:43:04 +0100

    Added scraper implementation and test

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,17 +1,101 @@
 package scraper
 
 import (
-	"fmt"
+	"context"
+	"io"
+	"time"
+
+	"github.com/dave/scrapy/scraper/getter"
+	"github.com/dave/scrapy/scraper/logger"
+	"github.com/dave/scrapy/scraper/parser"
+	"github.com/dave/scrapy/scraper/queuer"
 )
 
 type State struct {
+	Timeout time.Duration
+	Getter  getter.Interface
+	Parser  parser.Interface
+	Queuer  queuer.Interface
+	Logger  logger.Interface
 }
 
-func New() *State {
-	return &State{}
-}
+func (s *State) Start(ctx context.Context, url string) {
+	s.Logger.Init()
+	s.Queuer.Start(func(url string) {
 
-func (s *State) Start(url string) error {
-	fmt.Println(url)
-	return nil
+		ctx, cancel := context.WithTimeout(ctx, s.Timeout)
+		defer cancel()
+
+		// Log that the url has started processing
+		s.Logger.Start(url)
+
+		start := time.Now()
+
+		// Start the getter
+		c := s.Getter.Get(ctx, url)
+
+		// Wait for the getter to start streaming the contents, but respect context cancellation
+		var body io.ReadCloser
+		var code int
+		var err error
+		select {
+		case <-ctx.Done():
+			s.Logger.Error(url, ctx.Err())
+			return
+		case r := <-c:
+			err = r.Err
+			body = r.Body
+			code = r.Code
+		}
+
+		// Log error
+		if err != nil {
+			s.Logger.Error(url, err)
+			return
+		}
+
+		// Close body if it is non nil
+		if body != nil {
+			defer body.Close()
+		}
+
+		// Don't continue if the code is not 200
+		if code != 200 {
+			s.Logger.Finish(url, code, time.Now().Sub(start), 0, 0)
+			return
+		}
+
+		// Parse the body
+		urls, errs := s.Parser.Parse(url, body)
+
+		// Perhaps the parser ended early because of cancellation? If so, log the error.
+		select {
+		case <-ctx.Done():
+			s.Logger.Error(url, ctx.Err())
+			return
+		default:
+			// great!
+		}
+
+		// Log the finish event
+		s.Logger.Finish(url, code, time.Now().Sub(start), len(urls), len(errs))
+
+		// Queue all the resulting urls
+		for _, u := range urls {
+			if added, err := s.Queuer.Push(u); err != nil {
+				s.Logger.Error(u, err)
+			} else if added {
+				s.Logger.Queue(u)
+			}
+		}
+	})
+
+	if added, err := s.Queuer.Push(url); err != nil {
+		s.Logger.Error(url, err)
+	} else if added {
+		s.Logger.Queue(url)
+	}
+
+	s.Queuer.Wait()
+	s.Logger.Exit()
 }
--- scraper/scraper_test.go
+++ scraper/scraper_test.go
@@ -1,1 +1,62 @@
 package scraper
+
+import (
+	"testing"
+	"time"
+
+	"context"
+
+	"reflect"
+
+	"github.com/dave/scrapy/scraper/getter/mockgetter"
+	"github.com/dave/scrapy/scraper/logger/mocklogger"
+	"github.com/dave/scrapy/scraper/parser/mockparser"
+	"github.com/dave/scrapy/scraper/queuer/concurrentqueuer"
+)
+
+func TestScraper(t *testing.T) {
+	log := &mocklogger.Logger{}
+	s := &State{
+		Timeout: time.Second,
+		Getter: &mockgetter.Getter{
+			Results: map[string]mockgetter.Dummy{
+				"a": {
+					Body:    "a_body",
+					Latency: time.Millisecond,
+				},
+				"b": {
+					Body:    "b_body",
+					Latency: time.Millisecond,
+				},
+				"c": {
+					Body:    "c_body",
+					Latency: time.Millisecond,
+				},
+				"d": {
+					Body:    "d_body",
+					Latency: time.Millisecond,
+				},
+			},
+		},
+		Parser: &mockparser.Parser{
+			Results: map[string]mockparser.Dummy{
+				"a_body": {
+					Urls: []string{"b", "c"},
+				},
+				"c_body": {
+					Urls: []string{"d", "e"},
+				},
+			},
+		},
+		Queuer: &concurrentqueuer.Queuer{Length: 10, Workers: 1},
+		Logger: log,
+	}
+
+	s.Start(context.Background(), "a")
+
+	expected := []string{"queue a", "start a", "finish a: 200, 2, 0", "queue b", "queue c", "start b", "finish b: 200, 0, 0", "start c", "finish c: 200, 2, 0", "queue d", "queue e", "start d", "finish d: 200, 0, 0", "start e", "finish e: 404, 0, 0"}
+
+	if !reflect.DeepEqual(log.Log, expected) {
+		t.Errorf("unexpected log contents - found %#v", log.Log)
+	}
+}
