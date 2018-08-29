ac1b456ea5181d73d6911b4a6b7e9e4341fb3bf4
Dave Brophy <dave@brophy.uk>
2018-08-29 18:42:19 +0100

    Added deprecated notice to mock queuer

--- scraper/queuer/mockqueuer/queuer.go
+++ scraper/queuer/mockqueuer/queuer.go
@@ -6,6 +6,7 @@ import (
 )
 
 // Mock queuer executes the action as soon as the url is pushed
+// Deprecated: using the concurrent queuer with Workers: 1 gives reproducible results and correct log order
 
 type Queuer struct {
 	action func(url string)
