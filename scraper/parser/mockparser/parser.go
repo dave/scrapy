c4003a96bf2c5ae66837f98ae077c8246886eaa3
Dave Brophy <dave@brophy.uk>
2018-08-29 12:10:09 +0100

    Added mock parser and test

--- scraper/parser/mockparser/parser.go
+++ scraper/parser/mockparser/parser.go
@@ -1,1 +1,32 @@
 package mockparser
+
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+)
+
+type Parser struct {
+	Results map[string]Dummy
+}
+
+type Dummy struct {
+	Urls []string
+	Errs []string
+}
+
+func (p *Parser) Parse(body io.ReadCloser) (urls []string, errs []error) {
+	b, err := ioutil.ReadAll(body)
+	body.Close()
+	if err != nil {
+		return nil, []error{err}
+	}
+	result, ok := p.Results[string(b)]
+	if !ok {
+		return nil, nil
+	}
+	for _, e := range result.Errs {
+		errs = append(errs, errors.New(e))
+	}
+	return result.Urls, errs
+}
---
+++ scraper/parser/mockparser/parser_test.go
@@ -0,0 +1,28 @@
+package mockparser
+
+import (
+	"bytes"
+	"io/ioutil"
+	"reflect"
+	"testing"
+)
+
+// TODO: Quick test for mock parser - perhaps improve if have time.
+
+func TestParser(t *testing.T) {
+	p := &Parser{
+		Results: map[string]Dummy{
+			"a": {
+				Urls: []string{"b"},
+			},
+		},
+	}
+	urls, errs := p.Parse(ioutil.NopCloser(bytes.NewBufferString("a")))
+	expected := []string{"b"}
+	if !reflect.DeepEqual(urls, expected) {
+		t.Errorf("expected urls: %#v, found %#v", expected, urls)
+	}
+	if len(errs) > 0 {
+		t.Errorf("expected nil errs, found %#v", errs)
+	}
+}
