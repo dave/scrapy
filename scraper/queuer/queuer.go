eb1d711c625bd41005ed0e6d8812fef67980a23e
Dave Brophy <dave@brophy.uk>
2018-08-29 22:58:58 +0100

    Comments

--- scraper/queuer/queuer.go
+++ scraper/queuer/queuer.go
@@ -2,6 +2,6 @@ package queuer
 
 type Interface interface {
 	Start(func(string))        // Starts processing the queue.
-	Push(string) (bool, error) // Add a url to the queue. Returns false if the url was a duplicate.
+	Push(string) (bool, error) // Attempt to add a url to the queue. Returns true if the url was added (false if duplicate).
 	Wait()                     // Waits for all items to be processed before returning.
 }
