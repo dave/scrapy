0fdd9629229d5a24d489d3ba89157a9fc177bfc9
Dave Brophy <dave@brophy.uk>
2018-08-30 14:43:22 +0100

    Converted existing table tests to simple format

--- scraper/parser/htmlparser/parser_test.go
+++ scraper/parser/htmlparser/parser_test.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"net/url"
 	"reflect"
-	"sort"
 	"strings"
 	"testing"
 )
@@ -86,102 +85,64 @@ func TestNormalise(t *testing.T) {
 	}
 }
 
-var tests = map[string]testSpec{
-	"simple": {
-		body: `<a href="a"></a>`,
-		urls: []string{"a"},
-	},
-	"url error": {
-		body: `<a href=":"></a>`,
-		errs: []string{"parse :: missing protocol scheme"},
-	},
-	"complex html": {
-		body: `<body><p><a href="a"></a></p><table><td><a href="b"></a></td></table><!--<a href="c"></a>--></body>`,
-		urls: []string{"a", "b"},
-	},
-	"html and errors": {
-		body: `<body><a href=":"></a><p><a href="a"></a></p><div><a href="b"></a><a href="1:2"></a></div></body>`,
-		urls: []string{"a", "b"},
-		errs: []string{"parse :: missing protocol scheme", "parse 1:2: first path segment in URL cannot contain colon"},
-	},
-	"include function": {
-		body: `<a href="http://a.com/a"></a><a href="http://b.com/b"></a>`,
-		inc:  func(url *url.URL) bool { return url != nil && url.Host == "b.com" },
-		urls: []string{"http://b.com/b"},
-	},
-}
-
-type testSpec struct {
-	single, skip bool
-	body         string
-	urls         []string
-	errs         []string
-	inc          func(url *url.URL) bool
-}
-
-func (s testSpec) run(name string, t *testing.T) {
-	t.Helper()
-
-	p := &Parser{}
-
-	if s.inc != nil {
-		p.Include = s.inc
-	}
-
-	body := ioutil.NopCloser(bytes.NewBufferString(s.body))
-
-	urls, errs := p.Parse("", body)
-
-	if !reflect.DeepEqual(urls, s.urls) {
-		t.Errorf("%s: unexpected urls - got: %#v, expected: %#v", name, urls, s.urls)
-	}
-	var errorStrings []string
-	for _, e := range errs {
-		errorStrings = append(errorStrings, e.Error())
-	}
-	if !reflect.DeepEqual(errorStrings, s.errs) {
-		t.Errorf("%s: unexpected errors - got: %#v, expected: %#v", name, errorStrings, s.errs)
-	}
-}
-
 func TestParser(t *testing.T) {
-	var single bool
-	for name, test := range tests {
-		if test.single {
-			if single {
-				panic("two tests marked as single")
-			}
-			single = true
-			tests = map[string]testSpec{name: test}
-		}
-	}
-
-	// order tests by name to ensure consistent execution order
-	type named struct {
-		testSpec
+	tests := []struct {
 		name string
+		body string
+		urls []string
+		errs []string
+		inc  func(url *url.URL) bool
+	}{
+		{
+			name: "simple",
+			body: `<a href="a"></a>`,
+			urls: []string{"a"},
+		},
+		{
+			name: "url error",
+			body: `<a href=":"></a>`,
+			errs: []string{"parse :: missing protocol scheme"},
+		},
+		{
+			name: "complex html",
+			body: `<body><p><a href="a"></a></p><table><td><a href="b"></a></td></table><!--<a href="c"></a>--></body>`,
+			urls: []string{"a", "b"},
+		},
+		{
+			name: "html and errors",
+			body: `<body><a href=":"></a><p><a href="a"></a></p><div><a href="b"></a><a href="1:2"></a></div></body>`,
+			urls: []string{"a", "b"},
+			errs: []string{"parse :: missing protocol scheme", "parse 1:2: first path segment in URL cannot contain colon"},
+		},
+		{
+			name: "include function",
+			body: `<a href="http://a.com/a"></a><a href="http://b.com/b"></a>`,
+			inc:  func(url *url.URL) bool { return url != nil && url.Host == "b.com" },
+			urls: []string{"http://b.com/b"},
+		},
 	}
-	var ordered []named
-	for name, spec := range tests {
-		ordered = append(ordered, named{spec, name})
-	}
-	sort.Slice(ordered, func(i, j int) bool { return ordered[i].name < ordered[j].name })
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			p := &Parser{}
 
-	// run tests, skipping marked tests
-	var skipped bool
-	for _, spec := range ordered {
-		if spec.skip {
-			skipped = true
-			continue
-		}
-		spec.run(spec.name, t)
-	}
+			if test.inc != nil {
+				p.Include = test.inc
+			}
 
-	// fail in single mode or if any tests are skipped
-	if single {
-		t.Fatal("test passed, but failed because single mode is set")
-	}
-	if skipped {
-		t.Fatal("tests passed, but skipped some")
+			body := ioutil.NopCloser(bytes.NewBufferString(test.body))
+
+			urls, errs := p.Parse("", body)
+
+			if !reflect.DeepEqual(urls, test.urls) {
+				t.Errorf("unexpected urls - got: %#v, expected: %#v", urls, test.urls)
+			}
+			var errorStrings []string
+			for _, e := range errs {
+				errorStrings = append(errorStrings, e.Error())
+			}
+			if !reflect.DeepEqual(errorStrings, test.errs) {
+				t.Errorf("unexpected errors - got: %#v, expected: %#v", errorStrings, test.errs)
+			}
+		})
 	}
 }
--- scraper/scraper_test.go
+++ scraper/scraper_test.go
@@ -3,7 +3,6 @@ package scraper
 import (
 	"context"
 	"reflect"
-	"sort"
 	"testing"
 	"time"
 
@@ -13,172 +12,136 @@ import (
 	"github.com/dave/scrapy/scraper/queuer/concurrentqueuer"
 )
 
-var tests = map[string]testSpec{
-	"simple": {
-		get: map[string]mockgetter.Dummy{
-			"a": {Body: "a_body"},
-		},
-		parse:    map[string]mockparser.Dummy{},
-		expected: []string{"queue a", "start a", "finish a: 200, 0, 0"},
-	},
-	"simple parsed": {
-		get: map[string]mockgetter.Dummy{
-			"a": {Body: "a_body"},
-		},
-		parse: map[string]mockparser.Dummy{
-			"a_body": {Urls: []string{"b"}},
-		},
-		expected: []string{"queue a", "start a", "finish a: 200, 1, 0", "queue b", "start b", "finish b: 404, 0, 0"},
-	},
-	"queue full": {
-		length:  2,
-		workers: 1,
-		get: map[string]mockgetter.Dummy{
-			"a": {Body: "a_body"},
-		},
-		parse: map[string]mockparser.Dummy{
-			"a_body": {Urls: []string{"b", "c", "d"}},
-		},
-		expected: []string{"queue a", "start a", "finish a: 200, 3, 0", "queue b", "queue c", "error d: queue full", "start b", "finish b: 404, 0, 0", "start c", "finish c: 404, 0, 0"},
-	},
-	"duplicate": {
-		length:  2,
-		workers: 1,
-		get: map[string]mockgetter.Dummy{
-			"a": {Body: "a_body"},
-		},
-		parse: map[string]mockparser.Dummy{
-			"a_body": {Urls: []string{"b", "b"}},
-		},
-		expected: []string{"queue a", "start a", "finish a: 200, 2, 0", "queue b", "error b: duplicate url", "start b", "finish b: 404, 0, 0"},
-	},
-	"complex": {
-		get: map[string]mockgetter.Dummy{
-			"a": {Body: "a_body"},
-			"b": {Body: "b_body"},
-			"c": {Body: "c_body"},
-			"d": {Body: "d_body"},
-		},
-		parse: map[string]mockparser.Dummy{
-			"a_body": {Urls: []string{"b", "c"}},
-			"c_body": {Urls: []string{"d", "e"}},
-		},
-		expected: []string{"queue a", "start a", "finish a: 200, 2, 0", "queue b", "queue c", "start b", "finish b: 200, 0, 0", "start c", "finish c: 200, 2, 0", "queue d", "queue e", "start d", "finish d: 200, 0, 0", "start e", "finish e: 404, 0, 0"},
-	},
-	"timeout": {
-		timeout: time.Millisecond * 10,
-		get: map[string]mockgetter.Dummy{
-			"a": {Body: "a_body", Latency: time.Second},
-		},
-		parse:    map[string]mockparser.Dummy{},
-		expected: []string{"queue a", "start a", "error a: context deadline exceeded"},
-	},
-	"cancel": {
-		cancel: true,
-		get: map[string]mockgetter.Dummy{
-			"a": {Body: "a_body", Latency: time.Second},
-		},
-		parse:    map[string]mockparser.Dummy{},
-		expected: []string{"queue a", "start a", "error a: context canceled"},
-	},
-}
-
-type testSpec struct {
-	single, skip    bool
-	length, workers int
-	timeout         time.Duration
-	start           string
-	get             map[string]mockgetter.Dummy
-	parse           map[string]mockparser.Dummy
-	expected        []string
-	cancel          bool
-}
-
-func (s testSpec) run(name string, t *testing.T) {
-	t.Helper()
-
-	log := &mocklogger.Logger{}
-
-	timeout := time.Second
-	if s.timeout > 0 {
-		timeout = s.timeout
-	}
-
-	workers := 1
-	if s.workers > 0 {
-		workers = s.workers
-	}
-
-	length := 10
-	if s.length > 0 {
-		length = s.length
-	}
-
-	start := "a"
-	if s.start != "" {
-		start = s.start
-	}
-
-	ctx := context.Background()
-	if s.cancel {
-		var cancel context.CancelFunc
-		ctx, cancel = context.WithCancel(ctx)
-		cancel()
-	}
-
-	state := &State{
-		Timeout: timeout,
-		Getter:  &mockgetter.Getter{Results: s.get},
-		Parser:  &mockparser.Parser{Results: s.parse},
-		Queuer:  &concurrentqueuer.Queuer{Length: length, Workers: workers},
-		Logger:  log,
-	}
-
-	state.Start(ctx, start)
-
-	if !reflect.DeepEqual(log.Log, s.expected) {
-		t.Errorf("unexpected log contents - found %#v", log.Log)
-	}
-}
-
 func TestScraper(t *testing.T) {
-	var single bool
-	for name, test := range tests {
-		if test.single {
-			if single {
-				panic("two tests marked as single")
+	tests := []struct {
+		name            string
+		length, workers int
+		timeout         time.Duration
+		start           string
+		get             map[string]mockgetter.Dummy
+		parse           map[string]mockparser.Dummy
+		expected        []string
+		cancel          bool
+	}{
+		{
+			name: "simple",
+			get: map[string]mockgetter.Dummy{
+				"a": {Body: "a_body"},
+			},
+			parse:    map[string]mockparser.Dummy{},
+			expected: []string{"queue a", "start a", "finish a: 200, 0, 0"},
+		},
+		{
+			name: "simple parsed",
+			get: map[string]mockgetter.Dummy{
+				"a": {Body: "a_body"},
+			},
+			parse: map[string]mockparser.Dummy{
+				"a_body": {Urls: []string{"b"}},
+			},
+			expected: []string{"queue a", "start a", "finish a: 200, 1, 0", "queue b", "start b", "finish b: 404, 0, 0"},
+		},
+		{
+			name:    "queue full",
+			length:  2,
+			workers: 1,
+			get: map[string]mockgetter.Dummy{
+				"a": {Body: "a_body"},
+			},
+			parse: map[string]mockparser.Dummy{
+				"a_body": {Urls: []string{"b", "c", "d"}},
+			},
+			expected: []string{"queue a", "start a", "finish a: 200, 3, 0", "queue b", "queue c", "error d: queue full", "start b", "finish b: 404, 0, 0", "start c", "finish c: 404, 0, 0"},
+		},
+		{
+			name:    "duplicate",
+			length:  2,
+			workers: 1,
+			get: map[string]mockgetter.Dummy{
+				"a": {Body: "a_body"},
+			},
+			parse: map[string]mockparser.Dummy{
+				"a_body": {Urls: []string{"b", "b"}},
+			},
+			expected: []string{"queue a", "start a", "finish a: 200, 2, 0", "queue b", "error b: duplicate url", "start b", "finish b: 404, 0, 0"},
+		},
+		{
+			name: "complex",
+			get: map[string]mockgetter.Dummy{
+				"a": {Body: "a_body"},
+				"b": {Body: "b_body"},
+				"c": {Body: "c_body"},
+				"d": {Body: "d_body"},
+			},
+			parse: map[string]mockparser.Dummy{
+				"a_body": {Urls: []string{"b", "c"}},
+				"c_body": {Urls: []string{"d", "e"}},
+			},
+			expected: []string{"queue a", "start a", "finish a: 200, 2, 0", "queue b", "queue c", "start b", "finish b: 200, 0, 0", "start c", "finish c: 200, 2, 0", "queue d", "queue e", "start d", "finish d: 200, 0, 0", "start e", "finish e: 404, 0, 0"},
+		},
+		{
+			name:    "timeout",
+			timeout: time.Millisecond * 10,
+			get: map[string]mockgetter.Dummy{
+				"a": {Body: "a_body", Latency: time.Second},
+			},
+			parse:    map[string]mockparser.Dummy{},
+			expected: []string{"queue a", "start a", "error a: context deadline exceeded"},
+		},
+		{
+			name:   "cancel",
+			cancel: true,
+			get: map[string]mockgetter.Dummy{
+				"a": {Body: "a_body", Latency: time.Second},
+			},
+			parse:    map[string]mockparser.Dummy{},
+			expected: []string{"queue a", "start a", "error a: context canceled"},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			log := &mocklogger.Logger{}
+
+			timeout := time.Second
+			if test.timeout > 0 {
+				timeout = test.timeout
 			}
-			single = true
-			tests = map[string]testSpec{name: test}
-		}
-	}
 
-	// order tests by name to ensure consistent execution order
-	type named struct {
-		testSpec
-		name string
-	}
-	var ordered []named
-	for name, spec := range tests {
-		ordered = append(ordered, named{spec, name})
-	}
-	sort.Slice(ordered, func(i, j int) bool { return ordered[i].name < ordered[j].name })
+			workers := 1
+			if test.workers > 0 {
+				workers = test.workers
+			}
 
-	// run tests, skipping marked tests
-	var skipped bool
-	for _, spec := range ordered {
-		if spec.skip {
-			skipped = true
-			continue
-		}
-		spec.run(spec.name, t)
-	}
+			length := 10
+			if test.length > 0 {
+				length = test.length
+			}
 
-	// fail in single mode or if any tests are skipped
-	if single {
-		t.Fatal("test passed, but failed because single mode is set")
-	}
-	if skipped {
-		t.Fatal("tests passed, but skipped some")
+			start := "a"
+			if test.start != "" {
+				start = test.start
+			}
+
+			ctx := context.Background()
+			if test.cancel {
+				var cancel context.CancelFunc
+				ctx, cancel = context.WithCancel(ctx)
+				cancel()
+			}
+
+			state := &State{
+				Timeout: timeout,
+				Getter:  &mockgetter.Getter{Results: test.get},
+				Parser:  &mockparser.Parser{Results: test.parse},
+				Queuer:  &concurrentqueuer.Queuer{Length: length, Workers: workers},
+				Logger:  log,
+			}
+
+			state.Start(ctx, start)
+
+			if !reflect.DeepEqual(log.Log, test.expected) {
+				t.Errorf("unexpected log contents - found %#v", log.Log)
+			}
+		})
 	}
 }
