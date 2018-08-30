0cfaf9bb461fa52ea683ce48c1cc85edd5a356ec
Dave Brophy <dave@brophy.uk>
2018-08-30 12:18:02 +0100

    Refactor queuer and remove deprecated mockqueuer

--- scraper/queuer/concurrentqueuer/queuer.go
+++ scraper/queuer/concurrentqueuer/queuer.go
@@ -10,13 +10,13 @@ type Queuer struct {
 	Length  int // Max queue length
 	Workers int // Number of concurrent workers
 
-	seen        sync.Map       // Tracks the urls that have been pushed in the past
-	queue       chan string    // The queue
-	wg          sync.WaitGroup // Waitgroup tracking progress
-	initialised bool           // Ensures initialisation orderss
+	seen                  sync.Map       // Tracks the items that have been pushed in the past
+	queue                 chan string    // The queue of items waiting to process
+	queueWait, workerWait sync.WaitGroup // Waitgroup tracking queue and workers
+	initialised           bool           // Ensures initialisation order
 }
 
-func (q *Queuer) Start(action func(url string)) {
+func (q *Queuer) Start(action func(string)) {
 
 	if q.initialised {
 		panic("Start should only be called once")
@@ -27,28 +27,34 @@ func (q *Queuer) Start(action func(url string)) {
 
 	for i := 0; i < q.Workers; i++ {
 		go func() {
+
+			// Use a waitgroup to ensure we don't exit before the workers have finished exiting.
+			q.workerWait.Add(1)
+			defer q.workerWait.Done()
+
+			// Read from the queue channel and perform the action on each item
 			for u := range q.queue {
 				action(u)
-				q.wg.Done()
+				q.queueWait.Done()
 			}
 		}()
 	}
 }
 
-func (q *Queuer) Push(url string) error {
+func (q *Queuer) Push(payload string) error {
 
 	if !q.initialised {
 		panic("Start must be called before Push")
 	}
 
-	if _, loaded := q.seen.LoadOrStore(url, true); loaded {
+	if _, loaded := q.seen.LoadOrStore(payload, true); loaded {
 		return queuer.DuplicateError
 	}
 
 	select {
-	case q.queue <- url:
+	case q.queue <- payload:
 		// Url was added to the queue
-		q.wg.Add(1)
+		q.queueWait.Add(1)
 		return nil
 	default:
 		// queue was full - don't want to wait here...
@@ -58,5 +64,7 @@ func (q *Queuer) Push(url string) error {
 }
 
 func (q *Queuer) Wait() {
-	q.wg.Wait()
+	q.queueWait.Wait()  // wait for the queue to finish
+	close(q.queue)      // close the queue channel so workers will start to exit
+	q.workerWait.Wait() // wait for all workers to finish exiting
 }
---
+++ scraper/queuer/mockqueuer/README.md
@@ -0,0 +1,5 @@
+# mockqueuer
+
+Mock queuer executes the action as soon as the url is pushed
+
+Deprecated: using the concurrent queuer with Workers: 1 gives reproducible results and correct log order
